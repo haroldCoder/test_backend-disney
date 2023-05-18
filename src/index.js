9358fc226603395b51feab74974a31a7c947b999
haroldCoder <haroldc2005@gmail.com>
2023-05-18 09:52:44 -0500

    connection db

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const swaggerSetup = require('./swagger');
+const sequelize = require('./connection');
 
 const app = express();
 
-
+//conectar a swagger
 swaggerSetup(app);
 
-
+//iniciar el servidor
 app.listen(3500, ()=>{
-    console.log(`Server on port 3500`);
+    console.log(`Servidor en el puerto 3500`);
+
+    //confirmar conexion a la db
+    sequelize.authenticate()
+    .then(() => {
+        console.log('Conexión a la base de datos establecida correctamente.');
+    })
+    .catch((error) => {
+        console.error('Error al conectar a la base de datos:', error);
+    });
 })
